27c743fbdef0f6c24d52e7986842e23e60703b26
shubham <shubham@MBP-Mailjet-Shubham.lan>
2016-05-01 18:03:37 +0200

    contact + autres pages routes ok

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('pages/index', { title: 'Acceuil' });
+});
+
+router.get('/contact', function(req, res, next) {
+  res.render('pages/contact', { title: 'Contact' });
+});
+
+router.get('/collection', function(req, res, next) {
+  res.render('pages/collection', { title: 'Collection'});
+});
+
+router.get('/collection/hiver', function(req, res, next) {
+  res.render('pages/collection', { title: 'Collection', saison: 'hiver'});
+});
+
+router.get('/collection/ete', function(req, res, next) {
+  res.render('pages/collection', { title: 'Collection', saison: 'ete'});
+});
+
+router.get('/histoire', function(req, res, next) {
+  res.render('pages/histoire', { title: 'Histoire' });
 });
 
 module.exports = router;
